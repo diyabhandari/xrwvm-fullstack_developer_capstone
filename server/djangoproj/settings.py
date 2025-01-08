9fcc4c524d3286012678b8315c29130fa7532fea
diyabhandari <diyabhandari@gmail.com>
2025-01-08 02:36:08 -0500

    add module 3 task 2/3

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://diyabhandari-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://diyabhandari-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost','https://diyabhandari-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai','https://diyabhandari-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://diyabhandari-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai','https://diyabhandari-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
